dd720366cf0e04d2308a5dd933c7dae2d165a013
teo <tewodrosjob@gmail.com>
2026-08-09 14:35:09 +0300

    Revert "breake application comment"
    This reverts commit f973564f3c57c188f4ea2b7538bd01120a7339ea.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from './(tabs)/_layout.tsx'
+import AppTabs from '@/components/app-tabs';
 
 SplashScreen.preventAutoHideAsync();
 
